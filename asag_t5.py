0d555646e415c5ec875ddccad366302f766245c8
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2020-10-28 16:37:20 +0100

    first pytorch lightning try

--- asag_t5.py
+++ asag_t5.py
@@ -16,7 +16,7 @@ path = "datasets/preprocessed/sciEntsBank_train.npy"
 device = torch.device("cuda")
 
 # constants:
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 EPOCHS = 8
 tokenizer = T5Tokenizer.from_pretrained('t5-base')
 
@@ -70,19 +70,92 @@ for i in trange(EPOCHS, desc="Epoch "):
     model.eval()
     accuracy_point = 0
     validation_step = 0
+    label_truth = []
+    predictions = []
     for batch in val_loader:
         batch = tuple(t.to(device) for t in batch)
         text, lab = batch
         with torch.no_grad():
             output = tokenizer.decode(model.generate(text)[0])
         lab_comp = tokenizer.decode(lab.to('cpu').numpy().squeeze())
+        label_truth.append(lab_comp)
+        predictions.append(output)
         if output == lab_comp:
             accuracy_point += 1
 
         validation_step += 1
     accuracy = accuracy_point/validation_step
-    print("Accuracy: ", accuracy / validation_step)
+
+
+    def f1(pr, tr, class_num):
+        """
+        Calculates F1 score for a given class
+        :param pr: list of predicted values
+        :param tr: list of actual values
+        :param class_num: indicates class
+        :return: f1 score of class_num for predicted and true values in pr, tr
+        """
+
+        # Filter lists by class
+        pred = [x == class_num for x in pr]
+        truth = [x == class_num for x in tr]
+        mix = list(zip(pred, truth))
+        # Find true positives, false positives and false negatives
+        tp = mix.count((True, True))
+        fp = mix.count((False, True))
+        fn = mix.count((True, False))
+        # Return f1 score, if conditions are met
+        if tp == 0 and fn == 0:
+            recall = 0
+        else:
+            recall = tp / (tp + fn)
+        if tp == 0 and fp == 0:
+            precision = 0
+        else:
+            precision = tp / (tp + fp)
+        if recall == 0 and precision == 0:
+            return 0
+        else:
+            return 2 * recall * precision / (recall + precision)
+
+
+    def macro_f1(predictions, truth):
+        """
+        Calculates macro f1 score, where all classes have the same weight
+        :param predictions: logits of model predictions
+        :param truth: list of actual values
+        :return: macro f1 between model predictions and actual values
+        """
+
+        f1_0 = f1(predictions, truth, "incorrect")
+        f1_1 = f1(predictions, truth, "contradict")
+        f1_2 = f1(predictions, truth, "correct")
+        if np.sum([x == "contradict" for x in truth]) == 0:
+            return (f1_0 + f1_2) / 2
+        else:
+            return (f1_0 + f1_1 + f1_2) / 3
+
+
+    def weighted_f1(predictions, truth):
+        """
+        Calculates weighted f1 score, where all classes have different weights based on appearance
+        :param predictions: logits of model predictions
+        :param truth: list of actual values
+        :return: weighted f1 between model predictions and actual values
+        """
+
+        weight_0 = np.sum([x == "incorrect" for x in truth])
+        weight_1 = np.sum([x == "contradict" for x in truth])
+        weight_2 = np.sum([x == "correct" for x in truth])
+        f1_0 = f1(predictions, truth, "incorrect")
+        f1_1 = f1(predictions, truth, "contradict")
+        f1_2 = f1(predictions, truth, "correct")
+        return (weight_0 * f1_0 + weight_1 * f1_1 + weight_2 * f1_2) / len(truth)
+
+    print("Accuracy: ", accuracy)
+    print("Macro-F1: ", macro_f1(predictions, label_truth))
+    print("Weighted-F1: ", weighted_f1(predictions, label_truth))
     if accuracy/validation_step > tracker:
-        torch.save(model.state_dict(), "t5_testing_"+str(i)+"_"+str(accuracy*10000)[:2]+'.pt')
-        tracker = accuracy/validation_step
+        torch.save(model.state_dict(), "t5_testing_"+str(i)+"_"+str(accuracy*10000)[:3]+'.pt')
+        tracker = accuracy
 
---
+++ lit_asag_t5.py
@@ -0,0 +1,76 @@
+import os
+import torch
+import numpy as np
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split, RandomSampler
+from transformers import T5ForConditionalGeneration, Adafactor, T5Tokenizer
+from utils import macro_f1, weighted_f1
+import dataloading as dl
+import warnings
+warnings.filterwarnings("ignore")
+
+class LitT5(pl.LightningModule):
+
+    def __init__(self):
+        super(LitT5, self).__init__()
+        self.model = T5ForConditionalGeneration.from_pretrained('t5-large')
+        self.tokenizer = T5Tokenizer.from_pretrained('t5-base')
+        self.train_data, self.val_data = random_split(dl.SemEvalDataset("datasets/preprocessed/sciEntsBank_train.npy"),
+                                            [4472, 497], generator=torch.Generator().manual_seed(42))
+
+    def forward(self, tok_seq):
+        return self.tokenizer.decode(self.model.generate(tok_seq)[0])
+
+    def training_step(self, batch, batch_idx):
+        text, lab = batch
+        return self.model(input_ids=text, labels=lab)[0].mean()
+
+    def validation_step(self, batch, batch_idx):
+        text, lab = batch
+        return {'prediction': self(text), 'truth': self.tokenizer.decode(lab.to('cpu').numpy().squeeze())}
+
+    def validation_epoch_end(self, outputs):
+        pred = [x['prediction'] for x in outputs]
+        lab = [x['truth'] for x in outputs]
+        acc_stack = np.stack((pred, lab), axis=0)
+        acc = np.sum([1 for i in range(len(acc_stack.T)) if acc_stack[0, i] == acc_stack[1, i]]) / len(acc_stack.T)
+        m_f1 = macro_f1(pred, lab)
+        w_f1 = weighted_f1(pred, lab)
+        print("Accuracy: " + str(acc)[:6])
+        print("Macro-F1: " + str(m_f1)[:6])
+        print("Weighted-F1 " + str(w_f1)[:6])
+
+    """
+    # prepared for later use
+    def test_step(self, batch, batch_idx):
+        text, lab = batch
+        return {'prediction:': self(text), 'truth': lab}
+
+    def test_epoch_end(self, outputs):
+        pred = [x['prediction'] for x in outputs]
+        lab = [x['truth'] for x in outputs]
+        acc_stack = np.stack((pred, lab), axis=0)
+        acc = np.sum([1 for i in range(len(acc_stack.T)) if acc_stack[0, i] == acc_stack[1, i]]) / len(acc_stack.T)
+        m_f1 = macro_f1(pred, lab)
+        w_f1 = weighted_f1(pred, lab)
+        print("Accuracy: " + str(acc)[:6])
+        print("Macro-F1: " + str(m_f1)[:6])
+        print("Weighted-F1 " + str(w_f1)[:6])
+        return [acc, m_f1, w_f1]
+    """
+
+    def configure_optimizers(self):
+        return Adafactor(self.model.parameters(), lr=None, warmup_init=True, relative_step=True)
+
+    def train_dataloader(self):
+        train_sampler = RandomSampler(self.train_data)
+        return DataLoader(self.train_data, batch_size=4, num_workers=0, sampler=train_sampler)
+
+    def val_dataloader(self):
+        val_sampler = RandomSampler(self.val_data)
+        return DataLoader(self.val_data, batch_size=1, num_workers=0, shuffle=False, sampler=val_sampler)
+
+# Testing
+t5_test = LitT5()
+trainer = pl.Trainer(gpus=2, num_nodes=1, distributed_backend='ddp')
+trainer.fit(t5_test)
