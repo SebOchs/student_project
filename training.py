eb0e079a19d44c420e21206d94984ebcec72c28b
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2021-01-19 00:28:15 +0100

    t5 generation for server

--- training.py
+++ training.py
@@ -8,7 +8,7 @@ checkpoint_callback = ModelCheckpoint(
     filepath='models/kn1_t5_{epoch}-{bleu:.4f}',
     save_top_k=3
 )
-t5_test = LitT5(16, True)
+t5_test = LitT5(12, True)
 trainer = pl.Trainer(
     gpus=2,
     num_nodes=1,
