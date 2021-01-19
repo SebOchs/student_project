83baad4b9dc744a155bd804557f01a8da4876f2f
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2021-01-19 13:52:55 +0100

    ready for fine tune

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import pytorch_lightning as pl
 from lit_asag_t5 import LitT5
 
-t5_test = LitT5.load_from_checkpoint("models/kn1_t5_epoch=1-bleu=28.9119.ckpt")
+t5_test = LitT5.load_from_checkpoint("models/kn1_t5_epoch=21-bleu=48.9676.ckpt")
 trainer = pl.Trainer(
     gpus=1
 )
