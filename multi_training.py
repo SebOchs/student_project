e46178856c20e722820c33025c4c4e8f771e9566
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2021-01-29 10:42:10 +0100

    server out?

--- multi_training.py
+++ multi_training.py
@@ -8,13 +8,13 @@ checkpoint_callback = ModelCheckpoint(
     filepath='models/multi_kn1_t5_{epoch}-{my_metric:.4f}',
     save_top_k=3
 )
-t5_test = LitMultiT5(4)
+t5_test = LitMultiT5(2)
 trainer = pl.Trainer(
     gpus=2,
     num_nodes=1,
     accelerator='ddp',
     max_epochs=64,
-    accumulate_grad_batches=2,
+    accumulate_grad_batches=4,
     checkpoint_callback=checkpoint_callback,
     reload_dataloaders_every_epoch=True,
     num_sanity_val_steps=0,
